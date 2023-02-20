33fe32d91f5d0f67e491d2738f3d9099c1922483
Alejandr000 <87833679+Alejandr000@users.noreply.github.com>
2023-02-20 17:54:45 -0600

    Practica de arquitectura de capas

---
+++ PracticaArquitecturaCapas/CapaLogicaNegocios/CategoriaLogica.cs
@@ -0,0 +1,27 @@
+﻿using CapaAccesoDatos;
+using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogicaNegocios
+{
+    public class CategoriaLogica
+    {
+        //objeto de la clase CategoriaDatos
+        private CategoriaDatos Datos = new CategoriaDatos();
+
+        //metodo para la seleccion de datos
+        public DataTable ListaCategorias() {
+            return Datos.SelectCategoriaDatos();
+        }
+
+        //metodo para agrear una categoria
+        public bool AgregarCategoria(Categoria categoria) {
+            return Datos.InsertCategoria(categoria);
+        }
+    }
+}
